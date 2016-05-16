db24f991e5f4bbb08e4d8e98bfacac5eb14bccaa
Kate-peretyatko <Katrin-peretjatko@mail.ru>
2016-05-16 17:37:40 +0300

    Корреляция

--- untitled/fft.cpp
+++ untitled/fft.cpp
@@ -17,6 +17,7 @@ int fft(int argc, char **argv)
 
     complex_1d_array z = "[ 0.3827+0.9239i,  0.7071+0.7071i,  0.9239+0.3827i,   1.0000 ]";
     complex_1d_array y = "[ 1.5330e+005 +1.3392e+006i, 2.2845e+006 -2.1809e+006i, -1.9066e+006 -1.3138e+006i, -1.1442e+005 +1.4571e+006i ]";
+
     fftc1d(z);
     fftc1d(y);
     printf("%s\n", z.tostring(3).c_str()); // EXPECTED: [4i,0,0,0]
--- untitled/main.cpp
+++ untitled/main.cpp
@@ -1,4 +1,5 @@
 #include <QCoreApplication>
+#include "stdafx.h"
 #include <iostream>
 #include <fstream>
 #include <iomanip>
@@ -49,8 +50,8 @@ int main(int argc, char *argv[])
     std::complex <float> one(0, 1);
     for(int t = 1; t < 18; t++)
     {
-        twiddles[t] = exp(one * float((t - 1) *(M_PI/8)));
-        //cout << twiddles[t] << endl;
+         twiddles[t] = exp(one * float((t - 1) *(M_PI/8)));
+         //cout << twiddles[t] << endl;
     }
 
     /*complex <float> *down = new complex <float> [4];
@@ -72,42 +73,85 @@ int main(int argc, char *argv[])
     }*/
 
     std::complex <float> synchro[] = {twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
-                                      twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
-                                      twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
-                                      twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
-                                      twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
-                                      twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
-                                      twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
-                                      twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
-                                      twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
-                                      twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
-                                      twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
-                                      twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
-                                      twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
-                                      twiddles[6], twiddles[7], twiddles[8], twiddles[9], twiddles[10], twiddles[11], twiddles[12], twiddles[13],
-                                      twiddles[14], twiddles[15], twiddles[16], twiddles[17], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
-                                      twiddles[6], twiddles[7], twiddles[8], twiddles[9], twiddles[8], twiddles[7], twiddles[6], twiddles[5]};
+                                 twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
+                                 twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
+                                 twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
+                                 twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
+                                 twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
+                                 twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
+                                 twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
+                                 twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
+                                 twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
+                                 twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
+                                 twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
+                                 twiddles[4], twiddles[3], twiddles[2], twiddles[1], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
+                                 twiddles[6], twiddles[7], twiddles[8], twiddles[9], twiddles[10], twiddles[11], twiddles[12], twiddles[13],
+                                 twiddles[14], twiddles[15], twiddles[16], twiddles[17], twiddles[2], twiddles[3], twiddles[4], twiddles[5],
+                                 twiddles[6], twiddles[7], twiddles[8], twiddles[9], twiddles[8], twiddles[7], twiddles[6], twiddles[5]};
     for(int u = 0; u < 128; u++)
     {
         //cout << "u = " << u << " " << synchro[u]<< endl;
     }
-
     alglib::complex_1d_array z;
 
-    alglib::complex *pContent = new alglib::complex[128];
-    for(int u = 0; u < 128; u++){
-        pContent[u].x = synchro[u].real();
-        pContent[u].y = synchro[u].imag();
-    }
-    z.setcontent(128, pContent);
+        alglib::complex *pContent = new alglib::complex[128];
+        for(int u = 0; u < 128; u++){
+            pContent[u].x = synchro[u].real();
+            pContent[u].y = synchro[u].imag();
+        }
+        z.setcontent(128, pContent);
+        fftc1d(z);
+        pContent = z.getcontent();
+        for(int o = 0; o < 128; o++)
+        {
+            pContent[o] = alglib::conj(pContent[o]);
+            //printf("%s", pContent[o].tostring(3).c_str());
+        }
 
-    fftc1d(z);
+
+            alglib::complex_1d_array y;
+
+            alglib::complex *pSignal = new alglib::complex[32770];
+            for(int u = 0; u < 32770; u++){
+                pSignal[u].x = diff_com[u].real();
+                pSignal[u].y = diff_com[u].imag();
+            }
+            //printf("%s ", pSignal[0].tostring(3).c_str());
+            y.setcontent(32770, pSignal);
+            fftc1d(y);
+
+            pSignal = y.getcontent();
+            for(int o = 0; o < 30; o++)
+            {
+                //printf("%s ", pSignal[o].tostring(3).c_str());
+            }
+
+            alglib::complex *pSynchro = new alglib::complex[32770];
+            for(int u = 0; u < 128; u++){
+                pSynchro[u].x = pContent[u].x;
+                pSynchro[u].y = pContent[u].y;
+            }
+            for(int u = 128; u < 32770; u++){
+                pSynchro[u].x = 0;
+                pSynchro[u].y = 0;
+            }
+
+            alglib::complex_1d_array e;
+
+            alglib::complex *pMult = new alglib::complex[32770];
+            for( int h = 0; h < 32770; h++)
+            {
+                pMult[h].x = pSynchro[h].x*pSignal[h].x - pSynchro[h].y*pSignal[h].y;
+                pMult[h].y = pSynchro[h].x*pSignal[h].y + pSynchro[h].y*pSignal[h].x;
+            }
+            e.setcontent(32770, pMult);
+            fftc1dinv(e);
+            printf("%s ", e.tostring(3).c_str());
+
     std::cout << "USPECH" << std::endl;
-    printf("%s\n", z.tostring(3).c_str());
-
-
+    //std::cout << diff_com[0].real() << " " << diff_com[0].imag() << std::endl;
     QCoreApplication a(argc, argv);
-
+    //printf("%s", y.tostring(3).c_str());
     return a.exec();
 }
 
